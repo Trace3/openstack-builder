58a07ca12bbbe513ca3dc602ffb2dbdaf233490d
Ryan Benigno <ryanb@pobox.com>
2015-07-02 10:32:05 -0700

    Fix VMware Tools & Vagrant key

--- packer/scripts/update.sh
+++ packer/scripts/update.sh
@@ -4,7 +4,8 @@ apt-get update
 # apt-get install -qy open-vm-tools
 apt-get -qy dist-upgrade
 
-if [ -f /var/run/reboot-required ]; then
+# Hmm, reboot-required flag not always set after kernel update?
+#if [ -f /var/run/reboot-required ]; then
 	reboot
 	sleep 60
-fi
+#fi
